cf44c80ae1fc1951ad17feae888ca2cc4219fc29
Jevgeni <jevgeni1@hotmail.com>
2021-10-07 19:41:32 +0300

    Создали UI.Реализовали метод app.addBook()

--- src/myclasses/App.java
+++ src/myclasses/App.java
@@ -1,47 +1,77 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package myclasses;
+
 import entity.Author;
 import entity.Book;
-import entity.History;
-import entity.Reader;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.util.Scanner;
+
+
 /**
  *
- * @author jevge
+ * @author user
  */
 public class App {
+    private Scanner scanner = new Scanner(System.in);
+    private Book[] books = new Book[10];
     
-    
-    public void run (){
-        
-        Author author1 = new Author();
-        author1.setFirstname("Lev");
-        author1.setLastname("Tolstoy");
-        Author[] authors = new Author[1];
-        authors[0]=author1;
-        
-        Book book1 = new Book();
-        book1.setBookName("Voina i mir");
-        book1.setAuthors(authors);
-        book1.setPublishedYear(2021);
-        
-        Reader reader1 = new Reader();
-        reader1.setFirstname("Jevgeni");
-        reader1.setLastname("Filin");
-        reader1.setTelefone("58888761");
-        
-        History history1 = new History();
-        history1.setBook(book1);
-        history1.setReader(reader1);
-        Calendar c = new GregorianCalendar();
-        history1.setGivenDate(c.getTime());
-        
-        
+    public void run(){
+       String repeat = "r";
+        do{
+            System.out.println("Выберите номер задачи:");
+            System.out.println("0: Закончить программу");
+            System.out.println("1: Добавить книгу");
+            System.out.println("2: Список книг");
+            int task = scanner.nextInt(); scanner.nextLine();
+            switch (task) {
+                case 0:
+                    repeat="q";
+                    System.out.println("Пока!");
+                    break;
+                case 1:
+                    System.out.println("--- Добавление книги ---");
+                    for (int i = 0; i < books.length; i++) {
+                        if(books[i] == null){
+                            books[i] = addBook();
+                            break;
+                        }
+                        
+                    }
+                    break;
+                case 2:
+                    System.out.println("--- Список книг ---");
+                    for (int i = 0; i < books.length; i++) {
+                        if(books[i] != null){
+                            System.out.println(books[i].toString());
+                        }
+                        
+                    }
+                    System.out.println("-------------------");
+                    break;
+                default:
+                    System.out.println("Выберите цифру из списка!");;
+            }
+        }while("r".equals(repeat));
     }
-    
+    private Book addBook(){
+        Book book = new Book();
+        System.out.print("Введите название книги: ");
+        book.setBookName(scanner.nextLine());
+        System.out.print("Введите год издания книги: ");
+        book.setPublishedYear(scanner.nextInt());scanner.nextLine();
+        System.out.println("Введите автора книги ");
+        System.out.print("Сколько авторов у книги: ");
+        int countAuthors = scanner.nextInt();scanner.nextLine();
+        Author[] authors = new Author[countAuthors];
+        for (int i = 0; i < authors.length; i++) {
+            Author author = new Author();
+            System.out.println("Имя автора "+(i+1)+": ");
+            author.setFirstname(scanner.nextLine());
+            System.out.println("Фамилия автора: ");
+            author.setLastname(scanner.nextLine());
+            authors[i] = author;
+        }
+        book.setAuthors(authors);
+        return book;
+    }
+   
 }
