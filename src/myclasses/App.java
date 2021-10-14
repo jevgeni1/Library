d5d33bea9993cadef1a5c4119428f1d4ec6399fe
Jevgeni <jevgeni1@hotmail.com>
2021-10-14 17:42:56 +0300

    Реализована функция выдачи и возврата книги в библиотеку.

--- src/myclasses/App.java
+++ src/myclasses/App.java
@@ -7,7 +7,11 @@ package myclasses;
 
 import entity.Author;
 import entity.Book;
+import entity.History;
 import entity.Reader;
+import entity.Reader;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.Scanner;
 
 
@@ -19,6 +23,7 @@ public class App {
     private Scanner scanner = new Scanner(System.in);
     private Book[] books = new Book[10];
     private Reader[] readers = new Reader[10];
+    private History[] histories = new History[10];
     
     
     public void run(){
@@ -30,6 +35,8 @@ public class App {
             System.out.println("2: Список книг");
             System.out.println("3: Добавить читателя");
             System.out.println("4: Список читателей");
+            System.out.println("5: Выдать книгу читателю");
+            System.out.println("6: Вернуть книгу");
             int task = scanner.nextInt(); scanner.nextLine();
             switch (task) {
                 case 0:
@@ -74,7 +81,49 @@ public class App {
                     } 
                     System.out.println("-------------------");
                     break;
-                
+                case 5:
+                    System.out.println("--- Выдача книги ---");
+                    for (int i = 0; i < histories.length; i++) {
+                        if(histories[i] == null){
+                            histories[i] = addHistory();
+                            System.out.println("Книга "+histories[i].getBook().getBookName()
+                                                +" выдана читателю "+histories[i].getReader().getFirstname()
+                                                +" "+ histories[i].getReader().getLastname()
+                            );
+                            break;
+                        }
+                    }
+                    System.out.println("-------------------");
+                    break;
+                case 6:
+                    System.out.println("--- Возврат книги ---");
+                    System.out.println("Список читаемых книг:");
+                    int n = 0;
+                    for (int i = 0; i < histories.length; i++) {
+                        if(histories[i] != null && histories[i].getReturnedDate() == null){
+                            System.out.println(i+1+". Книгу "
+                                    +histories[i].getBook().getBookName()
+                                    +" читает "+histories[i].getReader().getFirstname()
+                                    +" "+histories[i].getReader().getLastname()
+                            );
+                            n++;
+                        }
+                    }
+                    if(n < 1){
+                        System.out.println("Нет читаемых книг!");
+                        System.out.println("-------------------");
+                        break;
+                    }
+                    System.out.print("Выберите номер возврщаемой книги: ");
+                    int numberHistory = scanner.nextInt(); scanner.nextLine();
+                    Calendar c = new GregorianCalendar();
+                    histories[numberHistory - 1].setReturnedDate(c.getTime());
+                    System.out.println("Книга "
+                            +histories[numberHistory - 1].getBook().getBookName()
+                            +" возвращена в библиотеку"
+                    );
+                    System.out.println("-------------------");
+                    break;
                 default:
                     System.out.println("Выберите цифру из списка!");;
             }
@@ -111,4 +160,42 @@ public class App {
         reader.setPhone(scanner.nextLine());
         return reader;
     }
+    private History addHistory() {
+        History history = new History();
+        System.out.println("Список книг:");
+        for (int i = 0; i < books.length; i++) {
+            if(books[i] != null){
+                StringBuilder sbAuthorNames = new StringBuilder();
+                for (int j = 0; j < books[i].getAuthors().length; j++) {
+                    sbAuthorNames.append(books[i].getAuthors()[j].getFirstname())
+                                    .append(" ")
+                                    .append(books[i].getAuthors()[j].getLastname())
+                                    .append(". ");
+                }
+                
+                System.out.println(i+1
+                        +". "+books[i].getBookName()
+                        +". "+books[i].getPublishedYear()
+                        +". "+sbAuthorNames.toString()
+                );
+            }
+        }
+        System.out.print("Выберите номер книги: ");
+        int numberBook = scanner.nextInt(); scanner.nextLine();
+        System.out.println("Список читателей:");
+        for (int i = 0; i < readers.length; i++) {
+            if(readers[i] != null){
+                System.out.println(i+1+". "+readers[i].toString());
+            }
+        }
+        System.out.print("Выберите номер читателя: ");
+        int numberReader = scanner.nextInt(); scanner.nextLine();
+        history.setBook(books[numberBook-1]);
+        history.setReader(readers[numberReader-1]);
+        Calendar c = new GregorianCalendar();
+        history.setGivenDate(c.getTime());
+        
+        return history;
+    }
+   
 }
